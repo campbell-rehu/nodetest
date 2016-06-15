ea263add5a23d35717afd208711692cc6be811a3
campbellrehu <campbellrehu@me.com>
2016-06-15 18:40:15 +1200

    Added app.use for static CSS stylesheets

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var port    = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req, res) {
   res.render("index");
