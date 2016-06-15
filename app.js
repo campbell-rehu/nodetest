4359ab9fb942f1bf3497d235981424757f5d16d1
campbellrehu <campbellrehu@me.com>
2016-06-15 17:59:29 +1200

    Update port

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app     = express();
+var port    = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
   res.send("This is the root route");
 });
 
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server connected...");
 });
