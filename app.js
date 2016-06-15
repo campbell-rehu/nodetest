cfaa76ed4a7f5dd7327c6db0d5fc7494d8d275a4
campbellrehu <campbellrehu@me.com>
2016-06-15 18:09:43 +1200

    Install ejs package, update view engine, set app.get to render index.ejs

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app     = express();
 var port    = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-  res.send("This is the root route");
+  res.render("index");
 });
 
 app.listen(port, function() {
