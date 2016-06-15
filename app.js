0b5049d0e34d4788a66c17995f5effddfc5ad08e
campbellrehu <campbellrehu@me.com>
2016-06-15 18:02:20 +1200

    Added Index.ejs file + body-parser to app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var app     = express();
 var port    = process.env.PORT || 3000;
+var bodyParser = require("body-parser");
 
 app.get("/", function(req, res) {
   res.send("This is the root route");
 });
-
 
 app.listen(port, function() {
   console.log("Server connected...");
